40441e9cadacdfff9dda0ddbe407097ee276c277
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-07-10 12:02:35 +0100

    Finish data_check component

--- main.py
+++ main.py
@@ -69,7 +69,17 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": config['data_check']['csv'],
+                    "ref": config['data_check']['ref'],
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
