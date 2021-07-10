26e1800a25ad68bc4cfc85e98de905d3633c4d6a
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-07-10 15:39:48 +0100

    Fix test failing for retraining of model

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,10 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    # Drop outliers in longitude and latitude columns
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review to datetime
     logger.info("Convert lastreview to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
